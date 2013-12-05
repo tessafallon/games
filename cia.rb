e0884d4828e9d3b89310e9ba6415347fd79fd4f9
contessa <tessa.fallon@gmail.com>
2013-12-05 18:18:25 -0500

    Population, inflation: done; continents in progress

--- cia.rb
+++ cia.rb
@@ -9,24 +9,32 @@ require 'awesome_print'
 		@xml = Nokogiri::XML(f)
 	end
 
-def population_high
-		test = @xml.xpath('//country/@population').collect {|population| population.text.to_i}
-		largest = test.sort.pop
-		p "The world's largest country population in 1996 was #{largest}." #if country pop = largest, return country name
-		#result = @xml.xpath('//country[@population="1210004956"]')
-		#p result
-	end
 
-def inflation_top5
-	p xml.search('//country').map{ |a| [a['name'], a['population']] }[0, 5]
+def inflation
+	all_count = xml.search('//country').map{ |a| [a['name'], a['inflation'].to_i] }
+	all_count.sort! {|a, b| a[1] <=> b[1]}
+	p all_count[-5, 5]
 end
 
-def highest_population
-	 test = @xml.xpath('//country/@population').each {|value| value}
-	 p test.sort
-	end
+def population
+	pop_array = []
+	 test = @xml.search('//country').collect {|a| [a['population'], a['name']] }
+	 test.each {|a, b| pop_array << a.to_i}
+	 max = pop_array.sort.last.to_s
+	 test.each {|a, b| (a == max) ? (puts "In 1996, #{b} had the world's largest population at #{a} people.") : nil}
+end
+
+def continents_with_countries
+	cntnts = @xml.search('/cia/continent').collect {|a| [a['name']]}
+	# cntnts.each {|name|
+	# 	name.each {|nm| nm.to_sym}} #3 retain quotes b/c of spacing: fix
+	main_has = Hash[cntnts.map{|name| name}]
+	countries = @xml.search('//country').collect {|a| [a['name'], a['continent']]}
+	p countries.each {|a, b| (b)} #put country in hash based on continent match
+end
+
 end
 
 cia_scraper = Scraper.new("cia-1996.xml")
 
- cia_scraper.inflation_top5
+ cia_scraper.continents_with_countries
