83cf2b74d20e26f3fc485ec4d9a692068b94c029
contessa <tessa.fallon@gmail.com>
2013-12-04 20:19:03 -0500

    Working time game

--- time_util.rb
+++ time_util.rb
@@ -1,8 +1,14 @@
 require 'date'
+require 'time'
 new_arr = []
 times = []
+more_arr = []
 arrivals = ["11:51pm", "11:56pm", "12:01am", "12:06am", "12:11am"]
-arrivals.each {|tme| new_arr << DateTime.parse(tme).strftime("%H:%M")}
-new_arr.each {|tme| times << tme.slice!(0..4).split(':')}
-p times.collect! {|a, b| b.to_i}
-p times. #add all together, avg 
+#arrivals.each {|tme| new_arr << DateTime.parse(tme).strftime("%H:%M")}
+arrivals.each {|tme| times << tme.slice!(0..4).split(':')}
+times.each {|a, b| more_arr << (a.to_i * 60) + (b.to_i)}
+sum_hrs = more_arr.inject{|sum,x| sum.to_f + x }
+sum_hrs
+average = ((sum_hrs/60/60/5)).round(2)
+final = average.to_s.gsub!('.', ':')
+p Time.parse(final)
